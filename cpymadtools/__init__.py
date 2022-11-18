ceb092093832bcdd8b607221c02a53065b6d591f
Felix Soubelet <felix.soubelet@protonmail.com>
2022-11-18 19:07:07 +0000

    fix imported name

--- cpymadtools/__init__.py
+++ cpymadtools/__init__.py
@@ -3,7 +3,7 @@ from . import _version, constants, coupling, lhc, matching, ptc, track, tune, tw
 __title__ = "cpymadtools"
 __description__ = "Lightweigth pythonic wrapper around cpymad, extracted from pyhdtoolkit"
 __url__ = "https://github.com/fsoubelet/cpymadtools"
-__version__ = _version.VERSION
+__version__ = _version.__version__
 __author__ = "Felix Soubelet"
 __author_email__ = "felix.soubelet@cern.ch"
 __license__ = "MIT"
