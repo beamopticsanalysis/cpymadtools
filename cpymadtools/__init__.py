d554c9bd9d59e156ef02bfe4ec0ca1dbaf9d0f57
Felix Soubelet <felix.soubelet@protonmail.com>
2022-11-18 18:55:13 +0000

    bit of info in init

--- cpymadtools/__init__.py
+++ cpymadtools/__init__.py
@@ -1,3 +1,11 @@
-from . import constants, coupling, lhc, matching, ptc, track, tune, twiss, utils
+from . import _version, constants, coupling, lhc, matching, ptc, track, tune, twiss, utils
+
+__title__ = "cpymadtools"
+__description__ = "Lightweigth pythonic wrapper around cpymad, extracted from pyhdtoolkit"
+__url__ = "https://github.com/fsoubelet/cpymadtools"
+__version__ = _version.VERSION
+__author__ = "Felix Soubelet"
+__author_email__ = "felix.soubelet@cern.ch"
+__license__ = "MIT"
 
 __all__ = [constants, coupling, lhc, matching, ptc, track, tune, twiss, utils]
